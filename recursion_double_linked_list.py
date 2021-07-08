df66818752b5ef965370df1e034b0a820cf688c7
brf08 <76757347+brf08@users.noreply.github.com>
2021-07-09 07:22:16 +0900

    双方向リスト完了

--- recursion_double_linked_list.py
+++ recursion_double_linked_list.py
@@ -28,6 +28,48 @@ class DoublyLinkedList:
         
         return iterator
     
+    # add the new node to the first of the list
+    def preappend(self, newNode):
+        self.head.prev = newNode
+        newNode.next = self.head
+        newNode.prev = None
+        self.head = newNode
+
+    # add the new node to the last of the list
+    def append(self, newNode):
+        self.tail.next = newNode
+        newNode.prev = self.tail
+        newNode.next = None
+        self.tail = newNode
+    
+    # add the new node to the next of the specified node
+    def addNextNode(self, node, newNode):
+        tempNode = node.next
+        node.next = newNode
+        newNode.next = tempNode
+        newNode.prev = node
+        
+        if node is self.tail: self.tail = newNode
+        else: tempNode.prev = newNode
+    
+    # pop the first node from the list
+    def popFront(self):
+        self.head = self.head.next
+        self.head.prev = None
+
+    # pot the last node from the list
+    def pop(self):
+        self.tail = self.tail.prev
+        self.tail.next = None
+
+    # delete the specified node from the list
+    def deleteNode(self, node):
+        if node is self.tail: return self.pop()
+        if node is self.head: return self.popFront()
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
     def reverse(self):
         reverse = self.tail
         iterator = self.tail.prev
@@ -75,5 +117,37 @@ numList.printInReverse()
 
 numList.printList()
 numList.reverse()
+numList.printList()
+numList.printInReverse()
+
+# 45をpreappend
+numList.preappend(Node(45))
+print(numList.head.data)
+numList.printList()
+
+# 71をappend
+numList.append(Node(71))
+print(numList.tail.data)
+numList.printList()
+print("")
+
+# ノードの後に新しいノードを追加
+numList.addNextNode(numList.at(3), Node(4))
+numList.printList()
+print(numList.tail.data)
+
+numList.addNextNode(numList.at(15), Node(679))
+numList.printList()
+print(numList.tail.data)
+
+numList.printInReverse()
+
+print("Deleting in O(1)")
+numList.printList()
+
+numList.deleteNode(numList.at(3))
+numList.deleteNode(numList.at(9))
+numList.deleteNode(numList.at(0))
+
 numList.printList()
 numList.printInReverse()
